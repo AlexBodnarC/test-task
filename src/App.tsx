5821fa126ebaad0e73420385c84ea3e125f52908
AlexBodnarC <alex.bodnar315@gmail.com>
2025-08-14 15:28:40 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-[980px]">
+      <div className="bg-black h-[800px] "></div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
